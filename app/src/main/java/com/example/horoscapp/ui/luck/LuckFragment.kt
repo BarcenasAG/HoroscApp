9ccf61a0c7a867ab1e0f5b3aabe705b7874b8c8e
BarcenasAG <alejandro.barcenas@shugar.mx>
2023-10-10 00:36:03 -0600

    ADD ViewModels and dependencies

--- app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.horoscapp.databinding.FragmentLuckBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LuckFragment : Fragment() {
+
+    private val luckViewModel by viewModels<LuckViewModel>()
 
     private var _binding:FragmentLuckBinding? = null
     private val binding get() = _binding!!
